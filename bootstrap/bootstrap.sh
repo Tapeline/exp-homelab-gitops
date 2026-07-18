30a86d73cc5b3047876cb55cae6e9acbfb373559
Tapeline <mail@tapeline.dev>
2026-07-18 15:47:52 +0500

    Initial

---
+++ bootstrap/bootstrap.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+echo "Deploy ArgoCD"
+kubectl create namespace argocd --dry-run=client -o yaml | kubectl apply -f -
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+echo "Waiting for ArgoCD"
+kubectl wait --for=condition=Available deployment/argocd-server -n argocd --timeout=300s
+
+echo "Configuring secrets store"
+kubectl create namespace external-secrets --dry-run=client -o yaml | kubectl apply -f -
+
+echo "Applying root"
+kubectl apply -f bootstrap/root-app.yml
+
+echo "Applied. Over"
